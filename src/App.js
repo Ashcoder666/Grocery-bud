c037192d9009fbc94e9c3bea18a182f078142a91
Ashir N Basheer <ashirnbasheer@gmail.com>
2022-02-16 18:54:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import List from './List'
+import Alert from './Alert'
+import './app.css'
 
-function App() {
+const App = () => {
+    const [name,setName] = useState('')
+    const [list,setList] = useState([])
+    const [isEditing,setIsEditing] = useState(false)
+    const [alert,setAlert] = useState({show:false,msg:'',type:''})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body-main' >
+      <h1 className='title' >Grocery Bud</h1>
+      <Alert />
+      <div  >
+          <input type='text' placeholder='eg: milk,egg ..' />
+          <button>ADD Item</button>
+      </div>
+      <List />
+      <button className='clr-btn' >Clear List</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/List.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const List = () => {
+  return (
+    <div>
+      LIst component
+    </div>
+  )
+}
+
+export default List
